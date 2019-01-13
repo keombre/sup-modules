cdcc45b89d8c02a30c573165d356f40c8d701f45
Jan Wijnhorst <wijnhjan@fit.cvut.cz>
2019-01-13 16:34:48 +0100

    PSR-2

--- controller/draw/api.php
+++ controller/draw/api.php
@@ -2,30 +2,35 @@
 
 namespace modules\lists\controller\draw;
 
-class api extends \sup\controller {
-
-    function __construct(\Slim\Container $container) {
+class api extends \sup\controller
+{
+    public function __construct(\Slim\Container $container)
+    {
         parent::__construct($container);
         $this->settings = $this->db->get("settings", "*");
     }
 
-    function __invoke($request, $response, $args) {
+    public function __invoke($request, $response, $args)
+    {
         return $response->withJson(['status' => 'error', 'code' => 1, 'message' => 'bad request'], 400);
     }
     
-    function lists($request, $response, $args) {
+    public function lists($request, $response, $args)
+    {
         foreach ($this->db->select('main', ['id', 'user'], [
             'version' => $this->settings['active_version'],
             'state' => 2
-        ]) as $entry)
+        ]) as $entry) {
             $students[] = [
                 'user' => (new \sup\User($this->container->base))->createFromDB(intval($entry['user']))->asArray(),
                 'list' => $entry['id']
             ];
+        }
         return $response->withJson(['status' => 'success', 'code' => 0, 'lists' => $students]);
     }
 
-    function draws($request, $response, $args) {
+    public function draws($request, $response, $args)
+    {
         if (!is_null($request->getQueryParam('all'))) {
             $draws = $this->db->select('draws', [
                 'id [Index]',
@@ -45,13 +50,15 @@ class api extends \sup\controller {
         return $response->withJson(['status' => 'success', 'code' => 0, 'draws' => $draws]);
     }
 
-    function books($request, $response, $args) {
+    public function books($request, $response, $args)
+    {
         $list = filter_var($args['list'], FILTER_SANITIZE_STRING);
         if (!$this->db->has('main', ['id' => $list], [
             'version' => $this->settings['active_version'],
             'state' => 2
-        ]))
+        ])) {
             return $response->withJson(['status' => 'error', 'code' => 2, 'message' => 'not found'], 404);
+        }
         
         $books = $this->db->select('lists', ['[>]books' => ['book' => 'id']], [
             'books.id [Index]',
@@ -62,23 +69,30 @@ class api extends \sup\controller {
         return $response->withJson(['status' => 'success', 'code' => 0, 'books' => $books]);
     }
 
-    function draw($request, $response, $args) {
+    public function draw($request, $response, $args)
+    {
         $list = filter_var($args['list'], FILTER_SANITIZE_STRING);
         $book = filter_var($args['book'], FILTER_SANITIZE_STRING);
 
-        if (!$this->db->has('main', ['id' => $list], [
-            'version' => $this->settings['active_version'],
-            'state' => 2
-        ]) || !$this->db->has('lists', ['book' => $book, 'list' => $list]))
+        if (
+            !$this->db->has('main', ['id' => $list], [
+                'version' => $this->settings['active_version'],
+                'state' => 2
+            ]) ||
+            !$this->db->has('lists', ['book' => $book, 'list' => $list])
+        ) {
             return $response->withJson(['status' => 'error', 'code' => 2, 'message' => 'not found'], 404);
+        }
         
-        if ($this->db->has('draws', ['list' => $list, 'version' => $this->settings['active_version']]))
+        if ($this->db->has('draws', ['list' => $list, 'version' => $this->settings['active_version']])) {
             return $response->withJson(['status' => 'error', 'code' => 3, 'message' => 'already drawn']);
+        }
 
         if (!is_null($request->getQueryParam('time'))) {
             $time = filter_var($request->getQueryParam('time'), FILTER_VALIDATE_INT);
-            if (!\is_numeric($time))
+            if (!\is_numeric($time)) {
                 return $response->withJson(['status' => 'error', 'code' => 1, 'message' => 'bad request'], 400);
+            }
         } else {
             $time = time();
         }
@@ -90,7 +104,8 @@ class api extends \sup\controller {
         }
     }
 
-    function revoke($request, $response, $args) {
+    public function revoke($request, $response, $args)
+    {
         $list = filter_var($args['list'], FILTER_SANITIZE_STRING);
         $book = filter_var($args['book'], FILTER_SANITIZE_STRING);
 
@@ -101,26 +116,25 @@ class api extends \sup\controller {
             ]) ||
             !$this->db->has('lists', ['book' => $book, 'list' => $list]) ||
             !$this->db->has('draws', ['book' => $book, 'list' => $list, 'version' => $this->settings['active_version']])
-        )
+        ) {
             return $response->withJson(['status' => 'error', 'code' => 2, 'message' => 'not found'], 404);
+        }
         
         if ($this->db->delete('draws', ['list' => $list, 'book' => $book, 'version' => $this->settings['active_version']])) {
             return $response->withJson(['status' => 'successs', 'code' => 0, 'message' => 'OK']);
         } else {
             return $response->withJson(['status' => 'error', 'code' => 4, 'message' => 'database error']);
         }
-        
     }
-    
 }
 
 /**
  * api schema:
- * 
+ *
  * /lists -> all lists with users
  * /draws[?all] -> list of alredy drawn books
  * /{list id} -> all books from list with attributes
  * /{list id}/draw/{book id}[?time] -> mark book as drawn with time
  * /{list id}/revoke/{book id} -> revoke draw
- * 
+ *
  */
