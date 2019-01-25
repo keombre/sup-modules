9f8dfcb80e0029fb1ddad83b4aeb76acbe03b4e5
keombre <keombre8@gmail.com>
2019-01-25 20:53:17 +0100

    initial structure

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace modules\subjects;
+
+final class init {
+    
+    private $container;
+
+    function __construct(\Slim\Container $container) {
+        $this->container = $container;
+
+        if (!$this->container->db->count("settings")) {
+            $this->container->db->insert("settings", [
+                "open_editing"  => 0,
+                "open_accepting" => 0
+            ]);
+        }
+    }
+}
