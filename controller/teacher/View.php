81ce518ebbeeb7c90f6edb581b90cbf55c25e8ef
keombre <keombre8@gmail.com>
2019-01-29 17:36:14 +0100

    langify

--- controller/teacher/View.php
+++ controller/teacher/View.php
@@ -10,24 +10,17 @@ class View extends Controller
 {
     public function __invoke(Request $request, Response $response, $args)
     {
-        if (array_key_exists('id', $args)) {
-            $listID = filter_var($args['id'], FILTER_SANITIZE_STRING);
-
-        //return (new \modules\lists\controller\preview($this->container))->withListID($listID)->preview($request, $response, $args);
-        } else {
-            $students = [];
-            foreach ($this->db->select('main', [
-                'id [Index]',
-                'user [Int]'
-            ], ['version' => $this->settings['active_version'], 'state' => 2]) as $entry) {
-                $students[] = [
-                    'user' => $this->container->factory->userFromID($entry['user']),
-                    'list' => $entry['id']
-                ];
-            }
-
-            $response = $this->sendResponse($request, $response, "teacher/view.phtml", ["students" => $students]);
+        $students = [];
+        foreach ($this->db->select('main', [
+            'id [Index]',
+            'user [Int]'
+        ], ['version' => $this->settings['active_version'], 'state' => 2]) as $entry) {
+            $students[] = [
+                'user' => $this->container->factory->userFromID($entry['user']),
+                'list' => $entry['id']
+            ];
         }
-        return $response;
+
+        return $this->sendResponse($request, $response, "teacher/view.phtml", ["students" => $students]);
     }
 }
